336f8f0ef26adad88d7e21fcb5797ca23f8d5178
Svarallo <Svarallo@companieshouse.gov.uk>
2019-10-22 12:34:23 +0100

    Added euro to permitted characters in string formatter

--- src/global/string.formatter.ts
+++ src/global/string.formatter.ts
@@ -1,7 +1,7 @@
 /*
  * String formatting functions
  */
-const REGEX_NON_PRINTABLE_CHARS: RegExp = /[^ -\xFF]+/g;
+const REGEX_NON_PRINTABLE_CHARS: RegExp = /[^ -\xFF€]+/g;
 
 export const removeNonPrintableChars = (inputStr: string): string => {
   if (inputStr) {
--- src/test/global/string.formatter.spec.unit.ts
+++ src/test/global/string.formatter.spec.unit.ts
@@ -3,10 +3,17 @@ import {removeNonPrintableChars} from "../../global/string.formatter";
 
 describe("String formatter tests", () => {
   it("should remove non printable chars", () => {
-    const dodgyString: string = "hello.\r\nThis string is \t dodgy!";
+    const dodgyString: string = "hello.\r\nThis string is €\t dodgy!";
     const formattedString: string = removeNonPrintableChars(dodgyString);
 
-    expect(formattedString).toEqual("hello. This string is   dodgy!");
+    expect(formattedString).toEqual("hello. This string is €  dodgy!");
+  });
+
+  it("should not remove printable chars", () => {
+    const notDodgyString: string = "hello. This string is not dodgy !@£$%^&*()€#¢§¶ªº";
+    const formattedString: string = removeNonPrintableChars(notDodgyString);
+
+    expect(formattedString).toEqual("hello. This string is not dodgy !@£$%^&*()€#¢§¶ªº");
   });
 
   it("should return original string if it is empty", () => {
