e3eb5d53c0651f6370be8b5c727e998184c19eff
Matthew Culpin <mculpin@companieshouse.gov.uk>
2019-12-17 15:50:13 +0000

    Naming change

--- src/client/date.formatter.ts
+++ src/client/date.formatter.ts
@@ -1,16 +1,16 @@
 import moment = require("moment");
 
 const DISPLAY_DATE_FORMAT: string = "D MMMM YYYY";
-const REASON_DATE_FORMAT: string = "YYYY-MM-DDTHH:mm:ss";
+const  REASON_DATE_TIME_FORMAT: string = "YYYY-MM-DDTHH:mm:ss";
 
 export const formatDateForDisplay = (inputDate: string): string => {
   return moment(inputDate).format(DISPLAY_DATE_FORMAT);
 };
 
 export const formatDateForReason = (day: string, month: string, year: string): string => {
-  return moment(`${day}-${month}-${year}`, "DD-MM-YYYY").format(REASON_DATE_FORMAT);
+  return moment(`${day}-${month}-${year}`, "DD-MM-YYYY").format(REASON_DATE_TIME_FORMAT);
 };
 
 export const formatDateForRequest = (inputDate: string): string => {
-  return moment(inputDate, "YYYY-MM-DD").format(REASON_DATE_FORMAT);
+  return moment(inputDate, "YYYY-MM-DD").format(REASON_DATE_TIME_FORMAT);
 };
