475c7c3df8d082635f3e9f93f7e24c0ed7a960ea
markpit <mpitten@companieshouse.gov.uk>
2020-08-07 16:41:48 +0100

    LFA-1925 Integrating patch api call when extension limit exceeded

--- src/controllers/company.details.controller.ts
+++ src/controllers/company.details.controller.ts
@@ -1,5 +1,10 @@
-import { NextFunction, Request, Response } from "express";
-import { ExtensionsCompanyProfile, getCompanyProfile, createExtensionRequest } from "../client/apiclient";
+import {NextFunction, Request, Response} from "express";
+import {
+  createExtensionRequest, ExtensionFullRequest,
+  ExtensionsCompanyProfile,
+  getCompanyProfile,
+  setExtensionRequestStatus,
+} from "../client/apiclient";
 import logger from "../logger";
 import * as sessionService from "../services/session.service";
 import * as errorMessages from "../model/error.messages";
@@ -7,6 +12,9 @@ import * as templatePaths from "../model/template.paths";
 import * as pageURLs from "../model/page.urls";
 import * as keys from "../session/keys";
 import {buildCompanySummaryListRows} from "../global/summary.list.rows.builder";
+import {ExtensionRequestStatus} from "../model/extension.request.status";
+import Session from "../session/session";
+import {IExtensionRequest, IUserProfile} from "../session/types";
 
 export const route = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const companyNumber: string = sessionService.getCompanyInContext(req.chSession);
@@ -32,27 +40,26 @@ export const route = async (req: Request, res: Response, next: NextFunction): Pr
   }
 };
 
+// POST submit handler
 export const confirmCompanyStartRequest = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const userProfile = req.chSession.userProfile();
+  const chSession: Session = req.chSession;
+  const userProfile: IUserProfile = chSession.userProfile() as IUserProfile;
+
   if (!userProfile) {
     return next(new Error("No user profile found - Session data is missing"));
   }
 
-  const companyNumber: string = sessionService.getCompanyInContext(req.chSession);
+  const companyNumber: string = sessionService.getCompanyInContext(chSession);
   if (!companyNumber) {
     return next(new Error(errorMessages.NO_COMPANY_NUMBER_IN_SESSION));
   }
 
   try {
-    const token: string = req.chSession.accessToken() as string;
+    const token: string = chSession.accessToken() as string;
     if (token) {
       const company: ExtensionsCompanyProfile = await getCompanyProfile(companyNumber, token);
 
-      if (!isExtensionDueDateWithinLimit(company)) {
-        logger.info("Company not eligibile for extension as limit period has been exceeded");
-        return res.redirect(pageURLs.EXTENSIONS_EXTENSION_LIMIT_REACHED);
-      }
-
+      // check if they are late filing
       const isDueDatePassed = checkDueDate(company);
       if (company.isAccountsOverdue || isDueDatePassed) {
         return res.render(templatePaths.ACCOUNTS_OVERDUE, {
@@ -60,6 +67,8 @@ export const confirmCompanyStartRequest = async (req: Request, res: Response, ne
           templateName: templatePaths.ACCOUNTS_OVERDUE,
         });
       }
+
+      // create the extension request
       if (!(await sessionService.hasExtensionRequest(req.chSession))) {
         const response = await createExtensionRequest(company, token);
         logger.info(`User ${userProfile.id} has successfully started an extension request for company ` +
@@ -70,6 +79,14 @@ export const confirmCompanyStartRequest = async (req: Request, res: Response, ne
           `for companyNumber ${companyNumber}`);
       }
       await sessionService.updateExtensionSessionValue(req.chSession, keys.ALREADY_SUBMITTED, false);
+
+      // check they haven't extended too far previously
+      //   - need the extension request to exist before updating its status
+      if (!isExtensionDueDateWithinLimit(company)) {
+        await setExtensionStatusToRejected(chSession, companyNumber, token);
+        return res.redirect(pageURLs.EXTENSIONS_EXTENSION_LIMIT_REACHED);
+      }
+
       return res.redirect(pageURLs.EXTENSIONS_CHOOSE_REASON);
     } else {
       return next(new Error("User access token is missing from session"));
@@ -79,18 +96,22 @@ export const confirmCompanyStartRequest = async (req: Request, res: Response, ne
   }
 };
 
-const isExtensionDueDateWithinLimit = (company: ExtensionsCompanyProfile): boolean => {
-  const dueDateString: string = company.accountsDue;
+const isExtensionDueDateWithinLimit = (companyProfile: ExtensionsCompanyProfile): boolean => {
+  const dueDateString: string = companyProfile.accountsDue;
   if (!dueDateString || dueDateString.trim().length === 0) {
+    logger.debug(`Company ${companyProfile.companyNumber} due date not found`);
     return true;
   }
   const dueDate: Date = new Date(dueDateString);
   // TODO 1927 get minus value from chs config
   const dueDateMinusLimitPeriod: Date = new Date(dueDate.setMonth(dueDate.getMonth() - 12));
   dueDateMinusLimitPeriod.setHours(0, 0, 0);
-  const endDate: Date = new Date(company.accountingPeriodEndOn);
+  const endDate: Date = new Date(companyProfile.accountingPeriodEndOn);
   endDate.setHours(0, 0, 0);
-  return dueDateMinusLimitPeriod < endDate;
+
+  const isDueDateWithinLimit: boolean = dueDateMinusLimitPeriod < endDate;
+  logger.debug(`Company ${companyProfile.companyNumber} isExtensionDueDateWithinLimit = ${isDueDateWithinLimit}`);
+  return isDueDateWithinLimit;
 };
 
 const checkDueDate = (company: ExtensionsCompanyProfile): boolean => {
@@ -99,4 +120,26 @@ const checkDueDate = (company: ExtensionsCompanyProfile): boolean => {
   const dueDate: Date = new Date(company.accountsDue);
   dueDate.setHours(23, 59, 59);
   return dueDate < currentDate;
+};
+
+const setExtensionStatusToRejected = async (chSession: Session, companyNumber: string, token: string) => {
+  logger.info("Company not eligible for extension as limit period has been exceeded");
+
+  const extensionRequest: IExtensionRequest = getExtensionRequest(chSession, companyNumber);
+
+  await setExtensionRequestStatus(
+    ExtensionRequestStatus.REJECTED_MAX_EXT_LENGTH_EXCEEDED,
+    extensionRequest.extension_request_id,
+    companyNumber,
+    token);
+};
+
+const getExtensionRequest = (chSession: Session, companyNumber: string): IExtensionRequest => {
+  const extensionRequest: IExtensionRequest = sessionService.getRequest(chSession);
+  if (!extensionRequest) {
+    const message = `Unable to retrieve extension request from session for company ${companyNumber}`;
+    logger.error(message);
+    throw new Error(message);
+  }
+  return extensionRequest;
 };
