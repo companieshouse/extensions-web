3b6968c59c3a6250d0af2da5cc77f7f3b1622ec8
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-03-18 13:47:03 +0000

    OBJ-492 Cookie Consent

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,7 @@ const env = nunjucks.configure([
     autoescape: true,
     express: app,
 });
+env.addGlobal("CHS_URL", process.env.CHS_URL);
 env.addGlobal("assetPath", process.env.CDN_HOST);
 env.addGlobal("ERROR_SUMMARY_TITLE", ERROR_SUMMARY_TITLE);
 env.addGlobal("PIWIK_URL", PIWIK_URL);
