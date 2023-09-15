f4bc5ac9616356bdd231a72d729175ce94c6ed27
Toba Folaranmi <ofolaranmi@companieshouse.gov.uk>
2023-09-15 12:44:36 +0100

    refactoring tf vars

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -1,6 +1,6 @@
 # Define all hardcoded local variable and local variables looked up from data resources
 locals {
-  stack_name                = "company-requests-service" # this must match the stack name the service deploys into
+  stack_name                = "company-requests" # this must match the stack name the service deploys into
   name_prefix               = "${local.stack_name}-${var.environment}"
   service_name              = "extensions-web"
   container_port            = "3000" # default node port required here until prod docker container is built allowing port change via env var
