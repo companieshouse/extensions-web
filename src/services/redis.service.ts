24e320bfa98eaeaf05b3c3c16e05a64cffff477f
markpit <mpitten@companieshouse.gov.uk>
2025-04-02 17:02:03 +0100

    LIVE issue - log out session signin_info data

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,3 +1,4 @@
+import logger from "../logger";
 import Session from "../session/session";
 import redisStore from "../session/store/redis.store";
 
@@ -6,6 +7,7 @@ import redisStore from "../session/store/redis.store";
  * @param chSession
  */
 const saveSession = async (chSession: Session): Promise<void> => {
+  logSignInInfo(chSession, "SAVING");
   await redisStore.save(chSession);
 };
 
@@ -15,8 +17,24 @@ const saveSession = async (chSession: Session): Promise<void> => {
  */
 const loadSession = async (cookieId: string): Promise<Session> => {
   const session: Session = Session.newWithCookieId(cookieId);
-  session.data = await redisStore.getData(session.sessionKey());
+  const sessionKey = session.sessionKey();
+
+  logger.info("src/services/redis.service.ts - LOADING session from Redis with cookieId = " + cookieId + ", sessionKey = " + sessionKey);
+  session.data = await redisStore.getData(sessionKey);
+  logSignInInfo(session, "LOADED", cookieId);
   return session;
+};
+
+const logSignInInfo = (chSession: Session, saveOrLoadText: String, cookieId?) => {
+  const copyOfSignInInfo = structuredClone(chSession["_data"]?.signin_info);
+
+  if (copyOfSignInInfo?.access_token?.access_token) {
+    copyOfSignInInfo.access_token.access_token = "*** HIDDEN ***";
+  }
+  if (copyOfSignInInfo?.access_token?.refresh_token) {
+    copyOfSignInInfo.access_token.refresh_token = "*** HIDDEN ***";
+  }
+  logger.info(`src/services/redis.service.ts - ${saveOrLoadText} session to/from Redis with sessionKey = ${chSession?.sessionKey()} , cookieId = ${cookieId}, signin_info = ${JSON.stringify(copyOfSignInInfo, null, 2)}`);
 };
 
 export {saveSession, loadSession};
