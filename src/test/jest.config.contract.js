e999b27b53279c9ab7d4d0b5a2e1e883265b862b
markpit <11330756+markpit@users.noreply.github.com>
2026-06-23 17:36:11 +0100

    ASM-2126 Fix latest CVEs

--- src/test/jest.config.contract.js
+++ src/test/jest.config.contract.js
@@ -1,17 +1,21 @@
 module.exports = {
-  roots: [
-    "<rootDir>"
-  ],
+  rootDir: "../../.",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          module: "commonjs",
+          target: "es6"
+        }
+      }
+    ],
+  },
+  testMatch: ["**/test/**/*.spec.contract.[jt]s"],
   testPathIgnorePatterns: [
     "/node_modules/",
     "/dist/"
   ],
-  preset: "ts-jest",
-  testEnvironment: "node",
-  verbose: true,
-  testMatch: ["**/test/**/*.spec.contract.[jt]s"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  globalSetup: "./global.setup.ts"
+  globalSetup: "./src/test/global.setup.ts"
 };
